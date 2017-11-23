7ffce56a21ddb08e1b14616f74d8e29f4157007a
Stéphane Amant <stephane.amant@oncodna.com>
2017-11-23 17:04:06 +0100

    Update middleware

--- airbrake/middleware.py
+++ airbrake/middleware.py
@@ -1,10 +1,15 @@
 from django.conf import settings
-from django.core.exceptions import MiddlewareNotUsed
+try:
+    # MiddlewareMixin is not available on older versions of Django
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    MiddlewareMixin = object
 from airbrake.utils.client import Client
 
-class AirbrakeNotifierMiddleware(object):
-    def __init__(self):
+class AirbrakeNotifierMiddleware(MiddlewareMixin):
+    def __init__(self, *args, **kwargs):
         self.client = Client()
+        super(AirbrakeNotifierMiddleware, self).__init__(*args, **kwargs)
 
     def process_exception(self, request, exception):
         if hasattr(settings, 'AIRBRAKE') and not settings.AIRBRAKE.get('DISABLE', False):
